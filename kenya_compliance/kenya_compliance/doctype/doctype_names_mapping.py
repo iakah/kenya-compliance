68c5b45225509131553f7b71690d1dbef69a71cd
Ian Kahare <37710249+iakah@users.noreply.github.com>
2024-02-28 12:18:32 +0300

    Update doctype_names_mapping.py

--- kenya_compliance/kenya_compliance/doctype/doctype_names_mapping.py
+++ kenya_compliance/kenya_compliance/doctype/doctype_names_mapping.py
@@ -3,11 +3,11 @@
 from typing import Final
 
 # Doctypes
-COMMUNICATION_KEYS_DOCTYPE_NAME: Final[str] = "Navari KRA eTims Communication Key"
-SETTINGS_DOCTYPE_NAME: Final[str] = "Navari KRA eTims Settings"
-LAST_REQUEST_DATE_DOCTYPE_NAME: Final[str] = "Navari KRA eTims Last Request Date"
-ROUTES_TABLE_DOCTYPE_NAME: Final[str] = "Navari KRA eTims Route Table"
-ROUTES_TABLE_CHILD_DOCTYPE_NAME: Final[str] = "Navari KRA eTims Route Table Item"
+COMMUNICATION_KEYS_DOCTYPE_NAME: Final[str] = "Bollex KRA eTims Communication Key"
+SETTINGS_DOCTYPE_NAME: Final[str] = "Bollex KRA eTims Settings"
+LAST_REQUEST_DATE_DOCTYPE_NAME: Final[str] = "Bollex KRA eTims Last Request Date"
+ROUTES_TABLE_DOCTYPE_NAME: Final[str] = "Bollex KRA eTims Route Table"
+ROUTES_TABLE_CHILD_DOCTYPE_NAME: Final[str] = "Bollex KRA eTims Route Table Item"
 
 # Global Variables
 SANDBOX_SERVER_URL: Final[str] = "https://etims-api-sbx.kra.go.ke/etims-api"
